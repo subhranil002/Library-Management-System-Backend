bfb4c38feb3b86d41be606eecb99961dfe5c9606
subhranil002 <106914208+subhranil002@users.noreply.github.com>
2024-05-05 23:50:11 +0530

    Changes in showing error message in production

--- src/middlewares/error.middleware.js
+++ src/middlewares/error.middleware.js
@@ -2,7 +2,7 @@ import constants from "../constants.js";
 
 const errorMiddleware = (err, req, res, next) => {
     // Get error details
-    const { statusCode, message, stack } = err;
+    let { statusCode, message, stack } = err;
 
     if (constants.NODE_ENV === "development") {
         console.log("ErrorMiddleware: ", err);
@@ -15,10 +15,21 @@ const errorMiddleware = (err, req, res, next) => {
         });
     }
 
+    // Capture the error message part only
+    const errorPattern = / Error: (.*)/;
+    const match = message.match(errorPattern);
+    if (match && match[1]) {
+        message = match[1].trim();
+    } else {
+        let parts = message.split("::");
+        console.log(parts);
+        message = parts[parts.length - 1].trim();
+    }
+
     // Send error response
     return res.status(statusCode).json({
         success: false,
-        message,
+        message
     });
 };
 

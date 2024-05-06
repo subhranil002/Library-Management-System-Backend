cfa99fc71ff1c504dfffed5e7b662eb11e86a321
subhranil002 <106914208+subhranil002@users.noreply.github.com>
2024-05-06 23:50:47 +0530

    Edited fineSchema ... Added paymentSchema

---
+++ src/models/payment.model.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+
+const paymentSchema = new mongoose.Schema(
+    {
+        razorpay_order_id: {
+            type: String,
+            required: true,
+            trim: true
+        },
+        razorpay_payment_id: {
+            type: String,
+            trim: true
+        },
+        amount: {
+            type: Number,
+            required: true
+        },
+        currency: {
+            type: String,
+            required: true
+        },
+        status: {
+            type: String,
+            required: true,
+            default: "CREATED",
+            enum: ["CREATED", "COMPLETED", "FAILED", "CANCELLED", "PAID"]
+        },
+        razorpay_signature: {
+            type: String,
+            trim: true
+        }
+    },
+    {
+        timestamps: true
+    }
+);
+
+export const Payment = mongoose.model("Payment", paymentSchema);

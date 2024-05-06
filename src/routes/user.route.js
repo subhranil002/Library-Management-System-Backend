6dd9394e78eff4a9855a1b844d7fe94a58df2902
subhranil002 <106914208+subhranil002@users.noreply.github.com>
2024-05-06 23:57:31 +0530

    Updated userRoutes

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -3,15 +3,17 @@ import {
     changeAvatar,
     changePassword,
     deleteUser,
+    fetchFineDetails,
     getBorrowedBooks,
     getCurrentUser,
+    getFineDetails,
     login,
     logout,
     refreshAccessToken,
     register,
     updateProfile
 } from "../controllers/user.controller.js";
-import { isLoggedIn } from "../middlewares/auth.middleware.js";
+import { authorizedRoles, isLoggedIn } from "../middlewares/auth.middleware.js";
 import upload from "../middlewares/multer.middleware.js";
 
 const userRouter = Router();
@@ -25,9 +27,15 @@ userRouter
     .route("/change-avatar")
     .put(upload.single("avatar"), isLoggedIn, changeAvatar);
 userRouter.route("/change-password").put(isLoggedIn, changePassword);
-userRouter.route("/update-profile").put(isLoggedIn, updateProfile);
+userRouter
+    .route("/update-profile")
+    .put(isLoggedIn, authorizedRoles("LIBRARIAN", "ADMIN"), updateProfile);
 userRouter.route("/refresh-token").get(refreshAccessToken);
 userRouter.route("/delete-user").delete(isLoggedIn, deleteUser);
 userRouter.route("/borrowed-books").get(isLoggedIn, getBorrowedBooks);
+userRouter.route("/fines").get(isLoggedIn, getFineDetails);
+userRouter
+    .route("/fines")
+    .post(isLoggedIn, authorizedRoles("LIBRARIAN", "ADMIN"), fetchFineDetails);
 
 export default userRouter;

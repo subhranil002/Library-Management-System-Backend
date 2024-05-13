accf50dce19ed71a43c7765cdaa9a835d2537448
subhranil002 <106914208+subhranil002@users.noreply.github.com>
2024-05-14 00:12:46 +0530

    Updated userRoutes

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -3,39 +3,104 @@ import {
     changeAvatar,
     changePassword,
     deleteUser,
-    fetchFineDetails,
+    fetchBorrowedBooks,
+    fetchFinedBooks,
     getBorrowedBooks,
     getCurrentUser,
-    getFineDetails,
+    getFine,
+    getFinedBooks,
     login,
     logout,
     refreshAccessToken,
     register,
-    updateProfile
+    sendOTP,
+    updateProfile,
+    verifyOTP
 } from "../controllers/user.controller.js";
-import { authorizedRoles, isLoggedIn } from "../middlewares/auth.middleware.js";
+import {
+    authorizedRoles,
+    isLoggedIn,
+    isVerified
+} from "../middlewares/auth.middleware.js";
 import upload from "../middlewares/multer.middleware.js";
 
 const userRouter = Router();
 
 // Routes
-userRouter.route("/register").post(register);
+userRouter
+    .route("/register")
+    .post(
+        isLoggedIn,
+        isVerified,
+        authorizedRoles("LIBRARIAN", "ADMIN"),
+        register
+    );
+
 userRouter.route("/login").post(login);
+
 userRouter.route("/logout").get(isLoggedIn, logout);
+
+userRouter
+    .route("/send-otp")
+    .post(
+        isLoggedIn,
+        isVerified,
+        authorizedRoles("LIBRARIAN", "ADMIN"),
+        sendOTP
+    );
+
+userRouter
+    .route("/verify-otp")
+    .post(
+        isLoggedIn,
+        isVerified,
+        authorizedRoles("LIBRARIAN", "ADMIN"),
+        verifyOTP
+    );
+
 userRouter.route("/current-user").get(isLoggedIn, getCurrentUser);
+
 userRouter
     .route("/change-avatar")
     .put(upload.single("avatar"), isLoggedIn, changeAvatar);
+
 userRouter.route("/change-password").put(isLoggedIn, changePassword);
+
 userRouter
     .route("/update-profile")
-    .put(isLoggedIn, authorizedRoles("LIBRARIAN", "ADMIN"), updateProfile);
+    .put(
+        isLoggedIn,
+        isVerified,
+        authorizedRoles("LIBRARIAN", "ADMIN"),
+        updateProfile
+    );
+
 userRouter.route("/refresh-token").get(refreshAccessToken);
-userRouter.route("/delete-user").delete(isLoggedIn, deleteUser);
-userRouter.route("/borrowed-books").get(isLoggedIn, getBorrowedBooks);
-userRouter.route("/fines").get(isLoggedIn, getFineDetails);
+
 userRouter
-    .route("/fines")
-    .post(isLoggedIn, authorizedRoles("LIBRARIAN", "ADMIN"), fetchFineDetails);
+    .route("/delete-user")
+    .delete(isLoggedIn, isVerified, authorizedRoles("ADMIN"), deleteUser);
+
+userRouter
+    .route("/borrowed-books")
+    .get(isLoggedIn, isVerified, getBorrowedBooks)
+    .post(
+        isLoggedIn,
+        isVerified,
+        authorizedRoles("LIBRARIAN", "ADMIN"),
+        fetchBorrowedBooks
+    );
+
+userRouter
+    .route("/fined-books")
+    .get(isLoggedIn, isVerified, getFinedBooks)
+    .post(
+        isLoggedIn,
+        isVerified,
+        authorizedRoles("LIBRARIAN", "ADMIN"),
+        fetchFinedBooks
+    );
+
+userRouter.route("/fine/:bookCode").get(isLoggedIn, isVerified, getFine);
 
 export default userRouter;

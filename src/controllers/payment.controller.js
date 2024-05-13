070835f64bdcff43071eddac3a37a9b2ff156054
subhranil002 <106914208+subhranil002@users.noreply.github.com>
2024-05-13 19:27:08 +0530

    Updated createPayment, verifyPayment ... Deleted completePayment

--- src/controllers/payment.controller.js
+++ src/controllers/payment.controller.js
@@ -7,7 +7,7 @@ import ApiError from "../utils/ApiError.js";
 import asyncHandler from "../utils/asyncHandler.js";
 import ApiResponse from "../utils/ApiResponse.js";
 import crypto from "crypto";
-import { differenceInDays } from "date-fns";
+import { endOfTomorrow } from "date-fns";
 
 export const getApiKey = asyncHandler(async (req, res) => {
     try {
@@ -29,40 +29,36 @@ export const getApiKey = asyncHandler(async (req, res) => {
 
 export const createPayment = asyncHandler(async (req, res, next) => {
     try {
-        // get payment details from params
-        const { book_transaction_id } = req.params;
+        // get payment details from request
+        const { bookCode, fineReason, fineAmount } = req.body;
 
         // Validate input data
-        if (!book_transaction_id) {
-            throw new ApiError("Book transaction id is required", 400);
+        if (!bookCode || !fineReason.trim() || !fineAmount) {
+            throw new ApiError("All fields are required", 400);
+        }
+        if (fineAmount <= 0) {
+            throw new ApiError("Fine amount must be a positive number", 400);
         }
 
         // find book transaction in database
         const bookTransaction = await BookTransaction.findOne({
-            _id: book_transaction_id,
-            status: "FINED"
+            "book.bookCode": bookCode,
+            status: {
+                $in: ["FINED", "PENDING"]
+            }
         });
         if (!bookTransaction) {
-            throw new ApiError("Invalid book transaction details", 400);
+            throw new ApiError("Book not issued yet", 400);
         }
 
         // Check if payment is already created
         const payment = await Payment.findOne({
             transaction_id: bookTransaction._id,
-            status: {
-                $in: ["CREATED", "COMPLETED", "PAID"]
-            }
+            status: "CREATED"
         });
-        if (payment) {
+        if (payment?.status === "CREATED") {
             throw new ApiError("Payment already created", 400);
         }
-
-        // calculate fine amount
-        const overdueDays = differenceInDays(
-            new Date(),
-            bookTransaction.returnDate
-        );
-        const fineAmount = overdueDays * constants.FINE_AMOUNT_PER_DAY;
 
         // Create payment options
         const options = {
@@ -78,18 +74,31 @@ export const createPayment = asyncHandler(async (req, res, next) => {
             });
 
         // Add orderId to database
-        await Payment.create({
+        const newPayment = await Payment.create({
             transaction_id: bookTransaction._id,
             razorpay_order_id: order.id,
             amount: order.amount,
             currency: order.currency,
-            status: order.status.toUpperCase()
+            status: "CREATED"
+        });
+
+        // Set book transaction as fined
+        bookTransaction.status = "FINED";
+        await bookTransaction.save();
+
+        // Save fine details to database
+        const fine = await Fine.create({
+            transaction: bookTransaction,
+            fineAmount: fineAmount,
+            fineReason: fineReason,
+            status: "CREATED",
+            payment: newPayment
         });
 
         // Send response
         return res
             .status(200)
-            .json(new ApiResponse("Payment created successfully", order));
+            .json(new ApiResponse("Payment created successfully", fine));
     } catch (error) {
         return next(
             new ApiError(
@@ -113,19 +122,11 @@ export const verifyPayment = asyncHandler(async (req, res, next) => {
 
         // Find order_id in database
         const payment = await Payment.findOne({
-            razorpay_order_id
+            razorpay_order_id,
+            status: "CREATED"
         });
         if (!payment) {
-            throw new ApiError("Invalid payment details", 400);
-        }
-
-        // Check if order is paid or failed or canceled
-        if (payment.status === "PAID") {
-            throw new ApiError("Already paid", 400);
-        } else if (payment.status === "FAILED") {
-            throw new ApiError("Payment failed, create a new payment", 400);
-        } else if (payment.status === "CANCELLED") {
-            throw new ApiError("Payment canceled, create a new payment", 400);
+            throw new ApiError("Invalid order id", 400);
         }
 
         // Generate signature
@@ -136,9 +137,7 @@ export const verifyPayment = asyncHandler(async (req, res, next) => {
 
         // Check if signature matches
         if (generatedSignature !== razorpay_signature) {
-            payment.status = "FAILED";
-            await payment.save();
-            throw new ApiError("Invalid payment details", 400);
+            throw new ApiError("Invalid razorpay signature", 400);
         }
 
         // Save payment details to database
@@ -147,52 +146,14 @@ export const verifyPayment = asyncHandler(async (req, res, next) => {
         payment.razorpay_signature = razorpay_signature;
         await payment.save();
 
-        // Send response
-        return res
-            .status(200)
-            .json(new ApiResponse("Payment verified successfully", payment));
-    } catch (error) {
-        return next(
-            new ApiError(
-                `payment.controller :: verifyPayment :: ${error}`,
-                error.statusCode
-            )
-        );
-    }
-});
-
-export const completePayment = asyncHandler(async (req, res, next) => {
-    try {
-        // Get payment details from request
-        const { razorpay_payment_id, book_transaction_id } = req.body;
-
-        // Validate input data
-        if (!razorpay_payment_id || !book_transaction_id) {
-            throw new ApiError("All fields are required", 400);
-        }
-
-        // Find payment in database
-        const payment = await Payment.findOne({
-            razorpay_payment_id,
-            status: "PAID"
-        });
-        if (!payment) {
-            throw new ApiError("Invalid payment id", 400);
-        }
-
         // Find book transaction in database
         const bookTransaction = await BookTransaction.findOne({
-            _id: book_transaction_id,
-            status: "FINED"
+            _id: payment.transaction_id
         });
-        if (!bookTransaction) {
-            throw new ApiError("Invalid book transaction details", 400);
-        }
 
-        // Update payment status
-        payment.status = "COMPLETED";
-        await payment.save();
-        bookTransaction.status = "PAID";
+        // Update transaction details
+        bookTransaction.status = "PENDING";
+        bookTransaction.returnDate = endOfTomorrow();
         await bookTransaction.save();
         const fine = await Fine.findOneAndUpdate(
             {
@@ -209,11 +170,16 @@ export const completePayment = asyncHandler(async (req, res, next) => {
         // Send response
         return res
             .status(200)
-            .json(new ApiResponse("Payment completed successfully", fine));
+            .json(
+                new ApiResponse(
+                    "Payment verified and completed successfully",
+                    fine
+                )
+            );
     } catch (error) {
         return next(
             new ApiError(
-                `payment.controller :: completePayment :: ${error}`,
+                `payment.controller :: verifyPayment :: ${error}`,
                 error.statusCode
             )
         );

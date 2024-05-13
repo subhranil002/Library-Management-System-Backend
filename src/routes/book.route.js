bcc8406b6c8b417bc17e2d7465fae07466a9de0b
subhranil002 <106914208+subhranil002@users.noreply.github.com>
2024-05-13 19:24:02 +0530

    Updated bookRoutes

--- src/routes/book.route.js
+++ src/routes/book.route.js
@@ -3,12 +3,17 @@ import {
     addBook,
     changeThumbnail,
     deleteBook,
+    forceReturnBook,
     getBookDetails,
     issueBook,
     returnBook,
     searchBooks
 } from "../controllers/book.controller.js";
-import { isLoggedIn, authorizedRoles } from "../middlewares/auth.middleware.js";
+import {
+    isLoggedIn,
+    authorizedRoles,
+    isVerified
+} from "../middlewares/auth.middleware.js";
 import upload from "../middlewares/multer.middleware.js";
 
 const bookRouter = Router();
@@ -16,25 +21,51 @@ const bookRouter = Router();
 // Routes
 bookRouter
     .route("/add-book")
-    .post(isLoggedIn, authorizedRoles("LIBRARIAN", "ADMIN"), addBook);
-bookRouter.route("/get-book/:isbn13").get(getBookDetails);
+    .post(
+        isLoggedIn,
+        isVerified,
+        authorizedRoles("LIBRARIAN", "ADMIN"),
+        addBook
+    );
+
 bookRouter.route("/search-books").get(searchBooks);
+
+bookRouter.route("/get-book/:isbn13").get(getBookDetails);
+
 bookRouter
     .route("/change-thumbnail/:bookCode")
     .put(
         isLoggedIn,
+        isVerified,
         authorizedRoles("LIBRARIAN", "ADMIN"),
         upload.single("thumbnail"),
         changeThumbnail
     );
+
 bookRouter
     .route("/delete-book/:bookCode")
-    .delete(isLoggedIn, authorizedRoles("ADMIN"), deleteBook);
+    .delete(isLoggedIn, isVerified, authorizedRoles("ADMIN"), deleteBook);
+
 bookRouter
     .route("/issue-book")
-    .post(isLoggedIn, authorizedRoles("LIBRARIAN", "ADMIN"), issueBook);
+    .post(
+        isLoggedIn,
+        isVerified,
+        authorizedRoles("LIBRARIAN", "ADMIN"),
+        issueBook
+    );
+
 bookRouter
     .route("/return-book/:bookCode")
-    .get(isLoggedIn, authorizedRoles("LIBRARIAN", "ADMIN"), returnBook);
+    .get(
+        isLoggedIn,
+        isVerified,
+        authorizedRoles("LIBRARIAN", "ADMIN"),
+        returnBook
+    );
+
+bookRouter
+    .route("/force-return-book/:bookCode")
+    .get(isLoggedIn, isVerified, authorizedRoles("ADMIN"), forceReturnBook);
 
 export default bookRouter;
